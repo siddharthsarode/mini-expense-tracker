6ffb59165856102a1a4225d3ffa7f51f449a357a
siddharthsarode <siddharthsarode279@gmail.com>
2024-10-12 15:07:06 +0530

    add new form validation

--- src/components/ExpenseForm/ExpenseForm.jsx
+++ src/components/ExpenseForm/ExpenseForm.jsx
@@ -1,83 +1,93 @@
-import React, { useState } from "react";
 import styles from "./ExpenseForm.module.css";
 import PrimaryButton from "../PrimaryButton/PrimaryButton";
-import SelectMenu from "../SelectMenu";
+
+import { useForm } from "react-hook-form";
 
 function ExpenseForm({ setExpenses }) {
-    const [formData, setFormData] = useState({
-        title: "",
-        category: "",
-        amount: "",
-        date: "",
-    });
-    const [error, setError] = useState({});
+    const {
+        register,
+        handleSubmit,
+        reset,
+        watch,
+        control,
+        formState: { errors, isSubmitting },
+    } = useForm();
 
-    const formConfig = {
-        title: [
-            { required: true, msg: "Title is Required." },
-            { minLength: 3, msg: "Title must have min 3 char" },
-        ],
-        category: [{ required: true, msg: "Category is Required." }],
-        amount: [
-            { required: true, msg: "Amount is Required" },
-            { pattern: /^\d+(.\d+)?$/, msg: "Invalid Amount" },
-        ],
-        date: [{ required: true, msg: "Date is Required." }],
-    };
-
-    const formValidate = (formData) => {
-        let errors = {};
-        Object.entries(formData).forEach(([key, value]) => {
-            // console.log(key, value);
-            formConfig[key]?.some((rule) => {
-                if (rule.required && !value) {
-                    errors[key] = rule.msg;
-                    return true;
-                }
-
-                if (rule.minLength > value.length) {
-                    errors[key] = rule.msg;
-                    return true;
-                }
-
-                if (rule.pattern && !rule.pattern.test(value)) {
-                    errors[key] = rule.msg;
-                    return true;
-                }
-            });
-        });
-
-        setError(errors);
-        return errors;
-    };
-
-    const handleInputChange = (event) => {
-        setFormData((prevState) => {
-            return { ...prevState, [event.target.name]: event.target.value };
-        });
-    };
-
-    const handleFormSubmit = (event) => {
-        event.preventDefault();
-        const errorObj = formValidate(formData);
-        if (Object.entries(errorObj).length > 0) return;
-
+    const onSubmit = async (formData) => {
         setExpenses((prevState) => {
             return [...prevState, { ...formData, id: crypto.randomUUID() }];
         });
-        setFormData({ title: "", category: "", amount: "", date: "" });
+        reset(); // clear form input  field
     };
+
+    // const formConfig = {
+    //     title: [
+    //         { required: true, msg: "Title is Required." },
+    //         { minLength: 3, msg: "Title must have min 3 char" },
+    //     ],
+    //     category: [{ required: true, msg: "Category is Required." }],
+    //     amount: [
+    //         { required: true, msg: "Amount is Required" },
+    //         { pattern: /^\d+(.\d+)?$/, msg: "Invalid Amount" },
+    //     ],
+    //     date: [{ required: true, msg: "Date is Required." }],
+    // };
+
+    // const formValidate = (formData) => {
+    //     let errors = {};
+    //     Object.entries(formData).forEach(([key, value]) => {
+    //         // console.log(key, value);
+    //         formConfig[key]?.some((rule) => {
+    //             if (rule.required && !value) {
+    //                 errors[key] = rule.msg;
+    //                 return true;
+    //             }
+
+    //             if (rule.minLength > value.length) {
+    //                 errors[key] = rule.msg;
+    //                 return true;
+    //             }
+
+    //             if (rule.pattern && !rule.pattern.test(value)) {
+    //                 errors[key] = rule.msg;
+    //                 return true;
+    //             }
+    //         });
+    //     });
+
+    //     setError(errors);
+    //     return errors;
+    // };
+
+    // const handleInputChange = (event) => {
+    //     setFormData((prevState) => {
+    //         return { ...prevState, [event.target.name]: event.target.value };
+    //     });
+    // };
+
+    // const handleFormSubmit = (event) => {
+    //     event.preventDefault();
+    //     const errorObj = formValidate(formData);
+    //     if (Object.entries(errorObj).length > 0) return;
+
+    //     setExpenses((prevState) => {
+    //         return [...prevState, { ...formData, id: crypto.randomUUID() }];
+    //     });
+    //     setFormData({ title: "", category: "", amount: "", date: "" });
+    // };
 
     const goBackPage = () => {
         window.history.back();
     };
+
+    const todayDate = new Date().toISOString().split("T")[0];
 
     return (
         <div className="container">
             <div className="row">
                 <div className="col-md-10 col-sm-12 mx-auto pb-3">
                     <h1 className="fs-2 mb-4 mt-5">Add a new expense</h1>
-                    <form onSubmit={handleFormSubmit}>
+                    <form onSubmit={handleSubmit(onSubmit)}>
                         <div className={styles.controls}>
                             <div className="row">
                                 <div className="col-sm-12 col-md-6">
@@ -91,12 +101,18 @@ function ExpenseForm({ setExpenses }) {
                                         <input
                                             type="text"
                                             name="title"
-                                            value={formData.title}
-                                            onChange={handleInputChange}
+                                            {...register("title", {
+                                                required: "This is a required",
+                                                minLength: {
+                                                    value: 3,
+                                                    message:
+                                                        "Title must have min 3 char",
+                                                },
+                                            })}
                                         />
-                                        {error && (
+                                        {errors.title && (
                                             <small className="error-text">
-                                                {error.title}
+                                                {errors.title.message}
                                             </small>
                                         )}
                                     </div>
@@ -109,28 +125,38 @@ function ExpenseForm({ setExpenses }) {
                                                 *
                                             </span>
                                         </label>
+                                        <select
+                                            {...register("category", {
+                                                required: "This is required",
+                                            })}
+                                        >
+                                            <option value="">
+                                                Select category
+                                            </option>
+                                            <option value="Food">Food</option>
+                                            <option value="Health">
+                                                Health
+                                            </option>
+                                            <option value="Entertainment">
+                                                Entertainment
+                                            </option>
+                                            <option value="Utilities">
+                                                Utilities
+                                            </option>
+                                            <option value="Utilities">
+                                                Travel
+                                            </option>
+                                        </select>
 
-                                        <SelectMenu
-                                            value={formData.category}
-                                            name="category"
-                                            onChange={handleInputChange}
-                                            defaultText={"Select Category"}
-                                            options={[
-                                                "Food",
-                                                "Transport",
-                                                "Entertainment",
-                                                "Health",
-                                                "Utilities",
-                                            ]}
-                                        />
-                                        {error && (
+                                        {errors.category && (
                                             <small className="error-text">
-                                                {error.category}
+                                                {errors.category.message}
                                             </small>
                                         )}
                                     </div>
                                 </div>
                             </div>
+
                             <div className="row mt-2">
                                 <div className="col-sm-12 col-md-6">
                                     <div className={styles.control}>
@@ -141,14 +167,18 @@ function ExpenseForm({ setExpenses }) {
                                             </span>
                                         </label>
                                         <input
-                                            type="number"
-                                            name="amount"
-                                            value={formData.amount}
-                                            onChange={handleInputChange}
+                                            type="text"
+                                            {...register("amount", {
+                                                required: "This is a required",
+                                                pattern: {
+                                                    value: /^\d+(.\d+)?$/,
+                                                    message: "Invalid amount",
+                                                },
+                                            })}
                                         />
-                                        {error && (
+                                        {errors.amount && (
                                             <small className="error-text">
-                                                {error.amount}
+                                                {errors.amount.message}
                                             </small>
                                         )}
                                     </div>
@@ -163,13 +193,18 @@ function ExpenseForm({ setExpenses }) {
                                         </label>
                                         <input
                                             type="date"
-                                            name="date"
-                                            value={formData.date}
-                                            onChange={handleInputChange}
+                                            {...register("date", {
+                                                required: "This is a required",
+                                                max: {
+                                                    value: todayDate,
+                                                    message:
+                                                        "You cannot select future date",
+                                                },
+                                            })}
                                         />
-                                        {error && (
+                                        {errors.date && (
                                             <small className="error-text">
-                                                {error.date}
+                                                {errors.date.message}
                                             </small>
                                         )}
                                     </div>
@@ -181,7 +216,9 @@ function ExpenseForm({ setExpenses }) {
                                     type="button"
                                     onClick={goBackPage}
                                 />
-                                <button type="submit">Add Expense</button>
+                                <button type="submit" disabled={isSubmitting}>
+                                    {isSubmitting ? "Adding" : "Add Expense"}
+                                </button>
                             </div>
                         </div>
                     </form>
